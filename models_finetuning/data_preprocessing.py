664a75fdea8a6c8975f84ec3d0aed7c08fc4ca26
marcel8168 <marcel_hiltner@protonmail.com>
2023-12-19 15:49:30 +0100

    [models-ft] add text preprocessing function to remove special characters etc

--- models_finetuning/data_preprocessing.py
+++ models_finetuning/data_preprocessing.py
@@ -1,3 +1,4 @@
+import re
 from xml.etree.ElementTree import ElementTree
 import pandas as pd
 from tqdm import tqdm
@@ -38,11 +39,25 @@ def xml_to_df(path: str, xml_file_names: list[str]):
                 print(f"An error occurred: {e}")
                 print(f"Error occured for PMID: {pmid}")
 
-            data_sets[i].append({'pmid': pmid, "text_types": text_types, 'title': title,
-                                 'abstract': abstract, 'meshtermlist': mesh_terms, 'labels': labels})
+            data_sets[i].append({'pmid': pmid, "text_types": text_types, 'title': preprocess_text(title),
+                                 'abstract': preprocess_text(abstract), 'meshtermlist': mesh_terms, 'labels': labels})
             progress_bar.update(1)
 
     hum_df = pd.DataFrame(data_sets[0])
     vet_df = pd.DataFrame(data_sets[1])
 
     return hum_df, vet_df
+
+
+def preprocess_text(text, lower_case=True, special_chars=True):
+    text_after_case_processing = text.lower() if lower_case else text
+
+    if special_chars:
+        text_after_tab_processing = re.sub(
+            r'[\r\n]+', ' ', text_after_case_processing)
+        text_after_special_chars_processing = re.sub(
+            r'[^\x00-\x7F]+', ' ', text_after_tab_processing)
+    else:
+        text_after_special_chars_processing = text_after_case_processing
+
+    return text_after_special_chars_processing
