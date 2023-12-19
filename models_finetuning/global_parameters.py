75fc508c3945bd9896c69a7903c7213430072826
marcel8168 <marcel_hiltner@protonmail.com>
2023-12-19 11:00:15 +0100

    [models_ft] change labels map and add path to saved models

--- models_finetuning/global_parameters.py
+++ models_finetuning/global_parameters.py
@@ -4,9 +4,9 @@
 
 NUM_LABELS = 2
 LABELS_MAP = {
-    0: "human_medicine", 
-    1: "veterinary_medicine"
-    }
+    "human_medicine": [1, 0],
+    "veterinary_medicine": [0, 1]
+}
 MAX_LEN = 512
 TRAIN_BATCH_SIZE = 4
 VAL_BATCH_SIZE = 4
@@ -14,3 +14,4 @@ TEST_BATCH_SIZE = 4
 EPOCHS = 2
 LEARNING_RATE = 1e-5
 RANDOM_SEED = 42
+PATH_SAVED_MODELS = "./saved_models/"
